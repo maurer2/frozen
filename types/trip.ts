d79cb76ec254bf09308117e8993c0a566712be07
Martin <mr.mmaurer+work@gmail.com>
2019-10-28 21:01:23 +0100

    Added types

---
+++ types/trip.ts
@@ -0,0 +1,17 @@
+declare module namespace {
+  export interface RootObject {
+    connection: boolean;
+    servicelevel: string;
+    internet: string;
+    speed: number;
+    gpsStatus: string;
+    tzn: string;
+    series: string;
+    latitude: number;
+    longitude: number;
+    serverTime: number;
+    wagonClass: string;
+    navigationChange: string;
+  }
+}
+
