70979dfb2e30be5481fb37b37a3da8d09fc958ad
Martin <mr.mmaurer+work@gmail.com>
2019-11-01 23:32:06 +0000

    Added default ink-app

---
+++ app/cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict';
+const React = require('react');
+const importJsx = require('import-jsx');
+const {
+  render
+} = require('ink');
+const meow = require('meow');
+
+const ui = importJsx('./ui');
+
+const cli = meow(`
+  Usage
+    $ app
+
+  Options
+      --name  Your name
+
+  Examples
+    $ app --name=Jane
+    Hello, Jane
+`);
+
+render(React.createElement(ui, cli.flags));
