d946edc3a806c1788b651d9af373d8e6d2b39afc
Martin <mr.mmaurer+work@gmail.com>
2019-11-02 23:52:53 +0000

    Added dummydata and added minimal layout

--- app/src/app.js
+++ app/src/app.js
@@ -1,14 +1,56 @@
-const React = require('react');
-const PropTypes = require('prop-types');
-const { Text, Color } = require('ink');
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+import {Text, Color, Box} from 'ink';
+import { readConfigurationFile } from 'tslint/lib/configuration';
+
+// dummyData
+const data = {
+  trip: {
+    trainType: 'ICE',
+    tripDate: '2019-10-27',
+    totalDistance: '512694',
+    stops: [
+      "Berlin Hbf",
+      "Berlin Südkreuz",
+      "Halle (Saale)",
+      "Erfurt",
+      "Kassel-Wilhelmshöhe",
+      "Hanau",
+      "Frankfurt (Main) Hbf",
+    ],
+  },
+  status: {
+    wagonClass: 'SECOND',
+    speed: 250,
+    latitude: '51.957258833333334',
+    longitude: '12.905845666666666',
+  }
+}
 
 const App = ({ name }) => (
-  <Text>
-    Hello,
-    <Color green>
-      { name}
-    </Color>
-  </Text>
+  <Box width="100%" padding={1}>
+    <Text>
+      Status
+    </Text>
+    <Box>
+      <dl>
+      {Object.keys(data.status).map((key) => (
+        <Fragment key={key}>
+          <dt>
+            <Color white>
+              {key}
+            </Color>
+          </dt>
+          <dd>
+            <Color red>
+              {data.status[key]}
+            </Color>
+          </dd>
+        </Fragment>
+        ))}
+      </dl>
+    </Box>
+  </Box>
 );
 
 App.propTypes = {
