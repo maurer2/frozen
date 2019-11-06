a258af5d5a40d1f8405d8d4c9b63ddc356b2fd44
Martin <mr.mmaurer+work@gmail.com>
2019-11-06 23:28:04 +0000

    Migrated from tslint to eslint for ts

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,15 +1,30 @@
 module.exports = {
+  root: true,
   extends: [
     "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
   ],
-  parser: "babel-eslint",
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    sourceType: "module"
+  },
   env: {
     node: true,
+    browser: true,
+  },
+  plugins: [
+    "@typescript-eslint",
+  ],
+  settings: {
+    "import/resolver": {
+      "node": {
+        "extensions": [
+          ".js",
+          ".ts",
+        ],
+      }
+    },
   },
   rules: {},
-  parserOptions: {},
-  plugins: [
-    "react"
-  ],
-  settings: {},
 };
