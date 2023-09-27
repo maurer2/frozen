4e8ef9f9d7c6123bc48f89e223869f775b4401fd
Martin <mr.mmaurer+work@gmail.com>
2023-09-27 02:09:02 +0200

    Added main loop, spinner and logo

---
+++ app2/src/app.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Text, useApp, Box, useInput} from 'ink';
+import Spinner from 'ink-spinner';
+
+import Header from './components/Header/Header.jsx';
+
+const acceptedKeys = ['q'] as const satisfies readonly string[];
+type AcceptedKeys = typeof acceptedKeys[number];
+
+export default function App() {
+  const {exit} = useApp();
+
+  useInput((input: AcceptedKeys | (string & {})): void => {
+    if (!((acceptedKeys as ReadonlyArray<string>).includes(input))) {
+      return;
+    }
+
+    if (input === 'q') {
+      exit();
+    }
+  });
+
+  return (
+    <>
+      <Box>
+        <Header width="100%"/>
+      </Box>
+      <Box>
+        <Text color="white">
+          <Spinner type="aesthetic" />
+          {' '}
+        </Text>
+        <Text>Press "q" to quit.</Text>
+      </Box>
+    </>
+  );
+}
---
+++ app2/src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import React, {ReactElement} from 'react';
+import {Box, BoxProps} from 'ink';
+import BigText from 'ink-big-text';
+
+type Props = {
+  width?: BoxProps['width'];
+}
+
+const Header = ({width}: Props): ReactElement  => {
+  const colors = ['red'];
+
+  return (
+    <Box marginBottom={2} width={width}>
+      <BigText
+        text="ICE-Status"
+        font="simple"
+        align="center"
+        colors={colors}
+        space={false}
+      />
+    </Box>
+  )
+};
+
+export default Header;
