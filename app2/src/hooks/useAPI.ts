546dea7de01aee9cf995dc3ccb3bbbeee1e410b4
Martin <mr.mmaurer+work@gmail.com>
2023-09-30 00:43:30 +0200

    Fixed statusSchema import

--- app2/src/hooks/useAPI.ts
+++ app2/src/hooks/useAPI.ts
@@ -1,28 +1,7 @@
 import { Zodios } from '@zodios/core';
 
-// import { statusSchema} from '../../../api/schemas/status/status.js'; // todo fix
+import statusSchemaImport from '../../../api/schemas/status/status.js';
 import type { StatusNew } from '../../../api/schemas/status/status.js';
-
-import { z } from "zod";
-
-// https://iceportal.de/api1/rs/status
-
-// temp workaround
-export const statusSchema = z
-  // non-strict to ignore superfluous keys and future key additions
-  .object({
-    connection: z.boolean(),
-    serviceLevel: z.enum(["AVAILABLE_SERVICE"]), // todo: find out about other values
-    gpsStatus: z.enum(["VALID", "INVALID"]),
-    internet: z.enum(["HIGH", "LOW"]),
-    latitude: z.number(),
-    longitude: z.number(),
-    speed: z.number().nonnegative(),
-    trainType: z.enum(["ICE", "IC"]),
-    tzn: z.string().min(2), // train number with train type
-    wagonClass: z.enum(["FIRST", "SECOND"]),
-    serverTime: z.number(),
-  });
 
 const useAPI = async (url: string): Promise<StatusNew> => {
   const apiClient = new Zodios(url, [
@@ -31,7 +10,7 @@ const useAPI = async (url: string): Promise<StatusNew> => {
       path: '/status',
       alias: 'getStatusData',
       description: 'Get status data from api',
-      response: statusSchema,
+      response: statusSchemaImport.statusSchema,
     },
   ]);
 
